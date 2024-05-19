02228c6f0d073ce23024021042343436badb5481
Caio Souza <caio.7s@outlook.com>
2024-05-18 21:02:00 -0300

    fix: Adjust deployment parameters

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express on Vercel');
 });
 
-// Endpoint de soma
+
 app.post('/add', (req: Request, res: Response) => {
   const { a, b } = req.body;
   if (typeof a !== 'number' || typeof b !== 'number') {
@@ -16,7 +18,37 @@ app.post('/add', (req: Request, res: Response) => {
   res.json({ result });
 });
 
-const PORT = 3001;
+app.post('/subtract', (req: Request, res: Response) => {
+  const { a, b } = req.body;
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return res.status(400).send('Both a and b should be numbers.');
+  }
+  const result = a - b;
+  res.json({ result });
+});
+
+app.post('/multiply', (req: Request, res: Response) => {
+  const { a, b } = req.body;
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return res.status(400).send('Both a and b should be numbers.');
+  }
+  const result = a * b;
+  res.json({ result });
+});
+
+app.post('/divide', (req: Request, res: Response) => {
+  const { a, b } = req.body;
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return res.status(400).send('Both a and b should be numbers.');
+  }
+  if (b === 0) {
+    return res.status(400).send('Division by zero is not allowed.');
+  }
+  const result = a / b;
+  res.json({ result });
+});
+
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server ready on port ${PORT}.`);
 });
